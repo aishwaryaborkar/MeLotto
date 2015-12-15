7c14e75b883fce6586c0ec8650ced6604ac381d2
Aishwarya <aishwarya.borkar@sjsu.edu>
2015-12-15 13:06:58 -0800

    added camera intent

--- app/src/main/java/com/example/aishwarya/melotto/DashboardActivity.java
+++ app/src/main/java/com/example/aishwarya/melotto/DashboardActivity.java
@@ -1,13 +1,18 @@
 package com.example.aishwarya.melotto;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
     TextView username;
+    Button startCamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,16 @@ public class DashboardActivity extends AppCompatActivity {
         Log.d("Welcome", welcome);
         username = (TextView) findViewById(R.id.txt_name);
         username.setText(welcome);
+
+        startCamera = (Button) findViewById(R.id.btn_camera);
+
+        startCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+
+            }
+        });
     }
 }
